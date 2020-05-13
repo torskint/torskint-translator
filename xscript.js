fa95662527ceed440d5f3cbef133dc2a1fbd7490
Tor Skint <tor.skint@gmail.com>
2020-05-13 12:01:22 +0200

    new update reverse

--- xscript.js
+++ xscript.js
@@ -18,20 +18,20 @@ function loadfile(filename, filetype){
 loadfile("//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit", "js");
 loadfile("https://cdn.jsdelivr.net/gh/torskint/torskint-translator/xstyle.css", "css");
 
-function createCookie(name, value, days, domain) {
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        var expires = "; expires=" + date.toGMTString();
-    } else {
-        var expires = "";
-    }
-    document.cookie = name + "=" + value + expires + "; domain=" + domain + "; path=/";
-}
+// function createCookie(name, value, days, domain) {
+//     if (days) {
+//         var date = new Date();
+//         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+//         var expires = "; expires=" + date.toGMTString();
+//     } else {
+//         var expires = "";
+//     }
+//     document.cookie = name + "=" + value + expires + "; domain=" + domain + "; path=/";
+// }
 
-function eraseCookie(name, domain) {
-    createCookie(name, "", -1, domain);
-}
+// function eraseCookie(name, domain) {
+//     createCookie(name, "", -1, domain);
+// }
 
 function googleTranslateElementInit() {
 	let tT = document.getElementById('torskint-translator')
@@ -41,7 +41,7 @@ function googleTranslateElementInit() {
 		ckDomain.shift();
 	}
 	ckDomain = ";domain=" + ckDomain.join(".");
-	eraseCookie("googtrans", "")
+	// eraseCookie("googtrans", "")
 	// domain cookie
 	// document.cookie = "googtrans=/"+lgParams.xlang+"/"+lgParams.ylang+"; expires=Thu, 07-Mar-2047 20:22:40 GMT; path=/" + ckDomain;
 	// host-only cookie (with no domain name definition)
