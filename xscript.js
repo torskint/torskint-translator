2517b5e9d7e06a6e45ea0be367ef46a55d5eb6f8
Tor Skint <tor.skint@gmail.com>
2020-05-13 11:51:55 +0200

    new update 1

--- xscript.js
+++ xscript.js
@@ -17,6 +17,22 @@ function loadfile(filename, filetype){
 }
 loadfile("//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit", "js");
 loadfile("https://cdn.jsdelivr.net/gh/torskint/torskint-translator/xstyle.css", "css");
+
+function createCookie(name, value, days, domain) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+    } else {
+        var expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; domain=" + domain + "; path=/";
+}
+
+function eraseCookie(name, domain) {
+    createCookie(name, "", -1, domain);
+}
+
 function googleTranslateElementInit() {
 	let tT = document.getElementById('torskint-translator')
 	const lgParams = tT.dataset;
@@ -25,6 +41,7 @@ function googleTranslateElementInit() {
 		ckDomain.shift();
 	}
 	ckDomain = ";domain=" + ckDomain.join(".");
+	eraseCookie("googtrans", "")
 	// domain cookie
 	// document.cookie = "googtrans=/"+lgParams.xlang+"/"+lgParams.ylang+"; expires=Thu, 07-Mar-2047 20:22:40 GMT; path=/" + ckDomain;
 	// host-only cookie (with no domain name definition)
